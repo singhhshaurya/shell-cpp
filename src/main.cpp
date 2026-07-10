fa7c5fc1ccfc902b570ec4ba1c3c15af22984d7b
Shaurya Singh <shaurya.2510.singh@gmail.com>
2026-07-10 17:21:24 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <functional>
 
 using namespace std;
 
+vector<string> split(string s, char delimeter=' '){
+	vector<string> ans;
+	int ptr = 0; 
 
-bool valid_command(string s){
-	return false;
+	for(int i=0; i<s.size(); i++){
+		if(s[i]==delimeter){
+			ans.push_back(s.substr(ptr, i-ptr));
+			ptr = i+1;
+		}	
+	}
+	ans.push_back(s.substr(ptr, s.size()-ptr));
+	return ans;
+
+}
+
+
+
+
+
+void echo(string& s){
+	cout << s;
+}
+
+unordered_map<string, function<void(string&)>> commands = {{"echo", echo}};
+
+
+
+
+void execute_line(string& command, string& args){
+	commands[command](args); // execute that 
+
+
+}
+
+bool invalid_command(string s){
+	return commands.find(s) == commands.end();
 }
 
 
@@ -21,14 +58,28 @@ int main() {
 		cout << "$ ";
 
 		if(!getline(cin, line)) break;
-		
 		if(line == "exit") break;
 
-		if(!valid_command(line)) cout << line << ": command not found";
+		
+		vector<string> tokens = split(line);
+		string command = tokens[0];
+		string args;
+
+		if(command.size()==line.size()) args = "";
+		else args = line.substr(command.size()+1, line.size() - command.size()-1);
+
+		if(invalid_command(command)) cout << command << ": command not found";
+		else execute_line(command, args);
+
+
 		cout << "\n";
 
 
+
+
 	}
+
+	return 1;
 
 }
 
