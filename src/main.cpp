0d71a41a30c0ec1d0170e9434d877594df195734
Shaurya Singh <shaurya.2510.singh@gmail.com>
2026-07-17 17:45:21 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -195,8 +195,8 @@ int main() {
 	while(true){
 		vector<string> commands_executed = {};
 
-		cout << curr_directory << "$ ";
-		// cout << "$ ";
+		// cout << curr_directory << "$ ";
+		cout << "$ ";
 		if(!getline(cin, line)) break;
 		commands_executed.push_back(line);
 
